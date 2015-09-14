fc165b42ee3bcd91fedfee6f12ce4526be26ba9b
EnriqueJosé <EnriqueJos@ULPGC-EJRBALMA>
2015-09-14 20:34:13 +0100

    version con Calendar

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -5,7 +5,8 @@
  */
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -19,13 +20,17 @@ public class Kata1 {
     public static void main(String[] args) {
 
         System.out.println("Inicio");
-        Person mipersona = new Person("Enrique", new Date(69, 5, 2));
+        Calendar c1 = GregorianCalendar.getInstance();
+        c1.set(1969,5,2);
+        Person mipersona = new Person("Enrique", c1);
         System.out.println(mipersona.getName());
-        System.out.println(mipersona.getBirthday());
+        System.out.println(mipersona.getBirthday().getTime());
         System.out.println(mipersona.getAge());
-        Person mipersona1 = new Person("Juan", new Date(64, 7, 3));
+        
+        c1.set(1964,7,3);
+        Person mipersona1 = new Person("Juan", c1);
         System.out.println(mipersona1.getName());
-        System.out.println(mipersona1.getBirthday());
+        System.out.println(mipersona1.getBirthday().getTime());
         System.out.println(mipersona1.getAge());
 
         System.out.println("Fin ");
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,14 +1,16 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+//import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Person {
 
     private final String name;
-    private final Date birthday;
+    private final Calendar birthday;
     private static final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
-    public Person(String name, Date birthday) {
+    public Person(String name, Calendar birthday) {
         this.name = name;
         this.birthday = birthday;
     }
@@ -17,13 +19,14 @@ public class Person {
         return name;
     }
 
-    public Date getBirthday() {
+    public Calendar getBirthday() {
         return birthday;
     }
+
     
     public int getAge(){
-        Date today = new Date();
-        return (int)(millisecondstoYear(today.getTime()-birthday.getTime()));
+        Calendar today = GregorianCalendar.getInstance();
+        return (int)(millisecondstoYear(today.getTimeInMillis() - birthday.getTimeInMillis()));
     }
     
     private int millisecondstoYear (long millis){
